1460c4887030cbc6870e98e5d7964974f0e9fd08
achebotar <a.chebotar@um-agency.com>
2018-10-16 17:16:38 +0300

    Threads & Rx

--- app/src/main/java/ru/chebotar/newyorktimesapp/presetation/feeds/FeedsFragment.java
+++ app/src/main/java/ru/chebotar/newyorktimesapp/presetation/feeds/FeedsFragment.java
@@ -1,14 +1,24 @@
 package ru.chebotar.newyorktimesapp.presetation.feeds;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.bumptech.glide.Glide;
+
+
+import java.util.concurrent.TimeUnit;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import io.reactivex.Observable;
+import io.reactivex.Scheduler;
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 import ru.chebotar.newyorktimesapp.R;
 import ru.chebotar.newyorktimesapp.data.test.model.DataUtils;
 import ru.chebotar.newyorktimesapp.data.test.model.NewsItem;
@@ -20,6 +30,7 @@ public class FeedsFragment extends BaseFragment {
     private RecyclerView recyclerView;
     private GridLayoutManager layoutManager;
     private FeedsAdapter adapter;
+    private ProgressBar progressBar;
 
     @Override
     protected int setLayoutRes() {
@@ -40,11 +51,24 @@ public class FeedsFragment extends BaseFragment {
     @Override
     protected void onPostCreateView() {
         recyclerView = rootView.findViewById(R.id.news_list);
+        progressBar = rootView.findViewById(R.id.progress_bar);
         recyclerView.setHasFixedSize(true);
         layoutManager = new GridLayoutManager(getActivity(), getResources().getInteger(R.integer.span_count));
         recyclerView.setLayoutManager(layoutManager);
-        adapter = new FeedsAdapter(DataUtils.generateNews(), Glide.with(getContext()), this::navigateToFeed);
+        adapter = new FeedsAdapter(Glide.with(getContext()), this::navigateToFeed);
         recyclerView.setAdapter(adapter);
+        getFeeds();
+    }
+
+    private void getFeeds() {
+        compositeDisposable.add(Single.just(DataUtils.generateNews())
+                .delay(2,TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe(disposable -> progressBar.setVisibility(View.VISIBLE))
+                .doAfterTerminate(() -> progressBar.setVisibility(View.GONE))
+                .subscribe(newsItems -> adapter.setData(newsItems))
+        );
     }
 
     private void navigateToFeed(NewsItem feed) {
